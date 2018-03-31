8edd0573e1fb37a2ac8bcfc0698c3ffe76bd0498
liuyunhe <lyhdyxd@vip.qq.com>
2018-03-31 15:49:40 +0800

    表格高度自适应

--- src/pages/tfgl/LandingPageSetting.vue
+++ src/pages/tfgl/LandingPageSetting.vue
@@ -14,9 +14,6 @@
             <el-form-item>
               <el-button type="primary" @click="getUsers">查询</el-button>
             </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="handleAdd">新增</el-button>
-            </el-form-item>
             <el-form-item class="fr mr0">
               <el-button icon="el-icon-plus">移动落地页</el-button>
             </el-form-item>
@@ -27,20 +24,31 @@
 
         </el-col>
         <!--列表-->
-        <el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
+        <el-table :data="users" :height='menuHeight' highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
             <el-table-column type="selection" width="55">
             </el-table-column>
             <el-table-column type="index" width="60">
             </el-table-column>
-            <el-table-column prop="name" label="姓名" width="120" sortable>
+            <el-table-column prop="name" label="姓名" width="120" >
             </el-table-column>
-            <el-table-column prop="sex" label="性别" width="100" :formatter="formatSex" sortable>
+
+              <el-table-column prop="sex" label="性别" width="100" :formatter="formatSex" >
+
+                <!--<el-dropdown trigger="hover">-->
+                <!--<el-dropdown-menu slot="dropdown">-->
+                  <!--<el-dropdown-item>账号：</el-dropdown-item>-->
+                  <!--<el-dropdown-item>角色：</el-dropdown-item>-->
+                  <!--<el-dropdown-item>归属：</el-dropdown-item>-->
+                <!--</el-dropdown-menu>-->
+                <!--</el-dropdown>-->
+              </el-table-column>
+
+
+            <el-table-column prop="age" label="年龄" width="100" >
             </el-table-column>
-            <el-table-column prop="age" label="年龄" width="100" sortable>
+            <el-table-column prop="birth" label="生日" width="120" >
             </el-table-column>
-            <el-table-column prop="birth" label="生日" width="120" sortable>
-            </el-table-column>
-            <el-table-column prop="addr" label="地址" min-width="180" sortable>
+            <el-table-column prop="addr" label="地址" min-width="180" >
             </el-table-column>
             <el-table-column label="操作" width="150">
                 <template scope="scope">
@@ -51,7 +59,13 @@
         </el-table>
         <!--工具条-->
         <el-col :span="24" class="footer-toolbar">
-            <el-pagination background layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total" style="float:right;">
+            <el-pagination
+              background
+              layout="prev, pager, next,jumper"
+              @current-change="handleCurrentChange"
+              :page-size="20"
+              :total="total"
+              style="float:right;">
             </el-pagination>
         </el-col>
     </section>
@@ -78,6 +92,7 @@
     import ElCol from "element-ui/packages/col/src/col";
     import ElFormItem from "../../../node_modules/element-ui/packages/form/src/form-item.vue";
     import ElButton from "../../../node_modules/element-ui/packages/button/src/button.vue";
+    import axios from 'axios'
 
     export default {
       components: {
@@ -87,7 +102,7 @@
       name: "",
         data() {
             return {
-
+                menuHeight:`${window.innerHeight - 285 > 300 ? window.innerHeight - 285 : 300}`,
                 listLoading: false,
                 filters: {
                   type: '',
@@ -101,6 +116,7 @@
             }
         },
         methods:{
+
           //获取用户列表
             getUsers() {
               console.log(this.filters)
@@ -120,9 +136,6 @@
             handleCurrentChange(val) {
                 this.page = val
                 this.getUsers()
-            },
-            handleAdd() {
-
             },
             selsChange() {
 
