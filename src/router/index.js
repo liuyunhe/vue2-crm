1a3c3d4e2ce4d5841d486099f64ecda5674d7146
liuyunhe <lyhdyxd@vip.qq.com>
2018-03-31 09:44:23 +0800

    目录添加Table页和404页

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Login from '../pages/Login.vue'
 import Home from '../pages/Home.vue'
+import NotFound from '../pages/404.vue'
+import Table from '../pages/Table.vue'
 
 Vue.use(Router)
 
@@ -16,8 +18,24 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
+      children:[
+        {
+          path: '/table',
+          name: 'Table',
+          component: Table,
+        }
+      ]
+    },
+    {
+      path:'/404',
+      name:'404',
+      component:NotFound
+    },
+    {
+      path: '*',
+      hidden: true,
+      redirect: { path: '/404' }
     }
-
   ]
 })
