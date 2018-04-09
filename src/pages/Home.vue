9409afd2ff3d552b802bd4ec3f30931c0d5323f9
liuyunhe <lyhdyxd@vip.qq.com>
2018-04-09 09:42:45 +0800

    添加首页

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,232 +1,90 @@
 <template>
-  <el-row class="container">
-    <el-col :span="24" class="header">
-      <el-col :span="4" class="userinfo">
-        <el-dropdown trigger="hover">
-          <span class="el-dropdown-link userinfo-inner"><img :src="this.sysUserAvatar" /> {{sysUserName}}</span>
-          <el-dropdown-menu slot="dropdown" class="user-dropdown-menu">
-            <el-dropdown-item disabled>账号：{{ sysAdmin }}</el-dropdown-item>
-            <el-dropdown-item disabled>角色：{{ '系统管理员' }}</el-dropdown-item>
-            <el-dropdown-item disabled>归属：{{ '集团总部' }}</el-dropdown-item>
-            <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
-      </el-col>
-      <el-col :span="4" class="logo-bar">
-        <span>全网导客平台</span>
-      </el-col>
-      <el-col  class="menu">
-        <el-menu
-          :default-active="activeIndex"
-          class="el-menu"
-          mode="horizontal"
-          router
-          @select="handleSelect"
-          background-color="#202A3E"
-          text-color="#fff"
-          active-text-color="#fff"
-        >
-          <!--单级菜单-->
-          <el-menu-item index="/">首页</el-menu-item>
-          <!--循环遍历多级菜单-->
-          <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-            <el-submenu :index="index+''" v-if="!item.leaf">
-              <template slot="title">{{item.name}}</template>
-              <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path" v-if="!child.hidden">{{child.name}}</el-menu-item>
-            </el-submenu>
-          </template>
-        </el-menu>
-      </el-col>
-    </el-col>
-    <el-col :span="24" class="main">
-      <section class="content-container">
-        <div class="grid-content bg-purple-light">
-          <el-col :span="24" class="content-wrapper">
-            <transition name="fade" mode="out-in">
-              <router-view></router-view>
-            </transition>
-          </el-col>
+  <div class="main-container">
+    <div class="index-container">
+      <div class="data-board">
+        <div class="data-board-zone">
+          <div class="title">展现量（uv）</div>
+          <div class="data-numb">22,330</div>
+          <div class="data-fluctuation">
+            <div class="item">昨日展现:<span>20,000</span><i class="add">11.50%</i></div>
+            <div class="item">日均展现:<span>20,000</span><i class="add">11.50%</i></div>
+            <div class="item">累计展现:<span>350,782</span><i class="add">11.50%</i></div>
+          </div>
         </div>
-      </section>
-    </el-col>
-  </el-row>
-
+        <div class="data-board-zone"></div>
+        <div class="data-board-zone"></div>
+        <div class="data-board-zone"></div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    import ElCol from "element-ui/packages/col/src/col";
+
     export default {
-        components: {ElCol},
-        name: "",
         data() {
             return {
-              breadcrumb:true,
-              //菜单
-              isCollapse:true,
-              isCollapseTransition:false,
-              //顶部导航
-              activeIndex: '',
-              //用户头像图片
-              sysUserAvatar:'',
-              //用户姓名
-              sysUserName:'',
-              //用户账号
-              sysAdmin:'',
+                msg: 'hello vue'
             }
         },
-        methods:{
-          logout() {
-            let _this = this
-
-            this.$confirm('确认要离开吗?','提示',{
-              confirmButtonText: '确定',
-              cancelButtonText: '取消',
-              type: 'warning'
-            })
-              .then(()=>{
-                sessionStorage.removeItem('user')
-                _this.$router.push('/login')
-              })
-              .catch(()=>{
-
-              })
-          },
-          handleSelect() {
-
-          }
-        },
-        mounted() {
-          let user = sessionStorage.getItem('user');
-          if(user) {
-            user = JSON.parse(user)
-            this.sysUserName = user.name || ''
-            this.sysUserAvatar = user.avatar || ''
-            this.sysAdmin = user.username || ''
-          }
-        }
     }
 </script>
 
-<style lang="scss" scoped>
-  @import "../styles/vars";
-  .el-menu-vertical-demo:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
-  }
-  .user-dropdown-menu{
-    .el-dropdown-menu__item.is-disabled{
-      color: #606266!important;
-    }
-  }
-  .container{
-    position: absolute;
-    top: 0px;
-    bottom: 0px;
-    width: 100%;
-    min-width: 1100px;
-    .header{
-      border-bottom: solid 1px #e6e6e6;
-      background: $color-top-menu;
-      color:#fff;
-      .logo-bar{
-        line-height: 66px;
-        font-size: 20px;
-        font-weight: bolder;
-      }
-      .menu {
-        /*background: $color-primary;*/
-        width: auto;
-        float:right;
-        height: 66px;
-        line-height: 66px;
-        padding: 4px 0;
-        .el-menu{
-          border: none;
-          box-sizing: border-box;
-          .el-menu-item{
-            float: left;
-            padding: 0 20px;
+<style lang="scss" >
+  .main-container{
+    width:100%;
+    background:rgba(249,250,251,1);
+    padding-top: 44px;
+    .index-container{
+      width: 1280px;
+      margin: 0 auto;
+      .data-board{
+        width: 100%;
+        height: 200px;
+        display: flex;
+        background:rgba(255,255,255,1);
+        border-radius: 2px;
+        .data-board-zone{
+          width: 25%;
+          padding: 30px 40px;
+          .title {
+            height: 20px;
+            line-height: 20px;
+            font-size: 14px;
+            color: #555;
           }
-          .el-submenu{
-
+          .data-numb{
+            height: 50px;
+            line-height: 50px;
+            color: #222;
+            font-size: 28px;
+            font-weight: bold;
+            margin-bottom: 10px;
           }
-        }
-      }
-      .userinfo {
-        text-align: right;
-        padding-right: 35px;
-        float: right;
-        line-height: 66px;
-        .userinfo-inner {
-          cursor: pointer;
-          color:#fff;
-          img {
-            width: 40px;
-            height: 40px;
-            border-radius: 20px;
-            margin: 13px 0px 13px 10px;
-            float: right;
+          .data-fluctuation{
+            .item{
+              height: 20px;
+              line-height: 20px;
+              color: #555;
+              font-size: 12px;
+              span{
+                margin-left: 10px;
+              }
+              i.add{
+                color: #E7343A;
+                font-size: 12px;
+                float: right;
+                &:before{
+                  content: "";
+                  border: 5px solid;
+                  border-color: transparent transparent #E7343A transparent;
+                  position: relative;
+                  top: -8px;
+                  margin: 5px;
+                }
+              }
+            }
           }
-        }
-      }
-    }
-    .main {
-      display: flex;
-      // background: #324057;
-      position: absolute;
-      top: 66px;
-      bottom: 0px;
-      overflow: hidden;
-      aside {
-        flex:0 0 230px;
-        width: 230px;
-        // position: absolute;
-        // top: 0px;
-        // bottom: 0px;
-        .el-menu{
-          height: 100%;
-        }
-        .collapsed{
-          width:60px;
-          .item{
-            position: relative;
-          }
-          .submenu{
-            position:absolute;
-            top:0px;
-            left:60px;
-            z-index:99999;
-            height:auto;
-            display:none;
-          }
-
-        }
-      }
-      .menu-collapsed{
-        flex:0 0 60px;
-        width: 60px;
-      }
-      .menu-expanded{
-        flex:0 0 230px;
-        width: 230px;
-        .el-menu.el-menu-vertical-demo{
-          width: inherit!important;
-        }
-      }
-      .content-container {
-        // background: #f1f2f7;
-        flex:1;
-        // position: absolute;
-        // right: 0px;
-        // top: 0px;
-        // bottom: 0px;
-        // left: 230px;
-        overflow-y: auto;
-        padding: 30px;
-
-        .content-wrapper {
-          background-color: #fff;
-          box-sizing: border-box;
         }
       }
     }
