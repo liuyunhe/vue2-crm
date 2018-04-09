b4e3ca99976eb3565a8d235a7eb1e5d711e4aab8
liuyunhe <lyhdyxd@vip.qq.com>
2018-04-09 09:43:08 +0800

    登陆后路由跳转首页

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -62,7 +62,7 @@
                     });
                   } else {
                     sessionStorage.setItem('user', JSON.stringify(user));
-                    this.$router.push({ path: '/LandingPageSetting' });
+                    this.$router.push({ path: '/Home' });
                   }
                 });
             }else {
