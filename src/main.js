b32bf039e43c08f92350a394912fea6a8cc9a874
liuyunhe <lyhdyxd@vip.qq.com>
2018-04-23 17:38:18 +0800

    ie add promise

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+//兼容ie
 import 'babel-polyfill'
+import promise from 'es6-promise';
+promise.polyfill();
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
