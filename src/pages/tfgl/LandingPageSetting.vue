c75254165273914b14ad95911be23c0e5a70bc17
liuyunhe <lyhdyxd@vip.qq.com>
2018-05-07 09:52:58 +0800

    落地页设置功能补全

--- src/pages/tfgl/LandingPageSetting.vue
+++ src/pages/tfgl/LandingPageSetting.vue
@@ -7,7 +7,9 @@
             {{ item.name }}
           </el-breadcrumb-item>
         </el-breadcrumb>
+        <el-button size="small fr">新增</el-button>
       </el-col>
+      <!--已发布/未上架-->
       <el-col :span="24" class="toolbar">
         <el-radio-group v-model="filters.status" :size="'small'" @change="getStatus">
           <el-radio-button label="0" >已发布</el-radio-button>
@@ -17,21 +19,41 @@
       <!--查询表单-->
       <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
         <el-form :inline="true" :model="filters">
-          <el-form-item label="条件查询">
-            <el-input :size="'small'" v-model="filters.name" placeholder="落地页ID、落地页名称、项目名称"></el-input>
+          <el-form-item>
+            <el-autocomplete
+              placeholder="搜索机构、项目、城市、落地页名称/ID"
+              v-model="filters.search"
+              class="input-with-select"
+              :fetch-suggestions="querySearch"
+              :trigger-on-focus="false"
+              ref="autocomplete"
+            >
+              <template slot-scope="{ item }">
+                <div class="name" style="width: 180px;float: left;overflow: hidden;text-overflow: ellipsis;white-space: nowrap;">{{ item.value }}</div>
+                <span class="addr" style="float: right;color: #99A9BF">{{ item.tag }}</span>
+              </template>
+              <el-select v-model="filters.type" slot="prepend" placeholder="请选择" class="input-left" :size="'small'">
+                <el-option label="全部" value="name"></el-option>
+                <el-option label="落地页ID" value="gid"></el-option>
+                <el-option label="落地页名称" value="pageName"></el-option>
+                <el-option label="所属项目" value="projectName"></el-option>
+                <el-option label="所属机构" value="orgName"></el-option>
+                <el-option label="城市" value="cityName"></el-option>
+              </el-select>
+            </el-autocomplete>
           </el-form-item>
-          <el-form-item label="所属城市">
-            <el-select :size="'small'" class="w120" v-model="filters.city" placeholder="请选择">
-              <el-option label="餐厅名" value="1"></el-option>
-              <el-option label="订单号" value="2"></el-option>
-              <el-option label="用户电话" value="3"></el-option>
-            </el-select>
-          </el-form-item>
+          <!--<el-form-item label="所属城市">-->
+            <!--<el-select :size="'small'" class="w120" v-model="filters.city" placeholder="请选择">-->
+              <!--<el-option label="餐厅名" value="1"></el-option>-->
+              <!--<el-option label="订单号" value="2"></el-option>-->
+              <!--<el-option label="用户电话" value="3"></el-option>-->
+            <!--</el-select>-->
+          <!--</el-form-item>-->
           <el-form-item label="投放时间">
             <el-date-picker
               value-format="yyyy-MM-dd"
               v-model="filters.time"
-              :size="'small'"
+              @change="configTime"
               type="daterange"
               range-separator="至"
               start-placeholder="开始日期"
@@ -46,7 +68,14 @@
           </el-form-item>
         </el-form>
       </el-col>
-
+      <!--已选标签-->
+      <el-col :span="24" class="toolbar">
+        <label class="el-form-item__label">已选条件：</label>
+        <div class="crm-tag clearfix" v-if="pageType">
+          <div class="ms">落地页类型:{{pageType==="1"?"移动落地页":"pc落地页"}}</div>
+          <div class="del" @click="deleteTag">X</div>
+        </div>
+      </el-col>
       <!--列表-->
       <el-col class="crm-table-wrap" v-loading="listLoading" :span="24" >
           <table class="crm-table" width="100%">
@@ -56,11 +85,22 @@
                 <th>落地页名称</th>
                 <th>所属项目</th>
                 <th>所属机构</th>
-                <th>项目所在城市</th>
-                <th>落地页类型</th>
+                <th>项目城市</th>
+                <th>
+                  <el-dropdown trigger="click" @command="pageTypeHandle">
+                    <span class="el-dropdown-link">
+                      落地页类型<i class="el-icon-arrow-down el-icon--right"></i>
+                    </span>
+                    <el-dropdown-menu slot="dropdown">
+                      <el-dropdown-item :class="{active:pageType === ''}" command="">全部</el-dropdown-item>
+                      <el-dropdown-item :class="{active:pageType === '0'}" command="0">PC落地页</el-dropdown-item>
+                      <el-dropdown-item :class="{active:pageType === '1'}" command="1">移动端落地页</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </th>
                 <th>更新时间</th>
                 <th>操作人员</th>
-                <th class="ctrl">操作</th>
+                <th class="ctrl" style="width: 270px">操作</th>
               </tr>
             </thead>
             <tbody>
@@ -70,25 +110,15 @@
                 <td :title="istitle(item.projectName)">{{item.projectName}}</td>
                 <td :title="istitle(item.orgName)">{{item.orgName}}</td>
                 <td :title="istitle(item.cityName)">{{ item.cityName }}</td>
-                <td >{{item.pagetype == 1 ? "移动落地页" : "PC落地页"}}</td>
+                <td >{{item.pageType == '1' ? "移动" : "PC"}}</td>
                 <td :title="istitle(item.updateTime)">{{item.updateTime}}</td>
                 <td :title="istitle(item.updateOperName)">{{item.updateOperName}}</td>
                 <td>
-                  <el-tooltip class="item" popper-class="table-tooltip" effect="dark" content="浏览" placement="top">
-                    <i class="table-icon ly"  size="small" @click="handleEdit(item)"></i>
-                  </el-tooltip>
-                  <el-tooltip class="item" popper-class="table-tooltip" effect="dark" content="发布" placement="top">
-                    <i class="table-icon fb" size="small" @click="handleEdit(item)"></i>
-                  </el-tooltip>
-                  <el-tooltip class="item" popper-class="table-tooltip" effect="dark" content="编辑" placement="top">
-                    <i class="table-icon bj" size="small" @click="handleEdit(item)"></i>
-                  </el-tooltip>
-                  <el-tooltip class="item" popper-class="table-tooltip" effect="dark" content="删除" placement="top">
-                    <i class="table-icon sc" size="small" @click="handleEdit(item)"></i>
-                  </el-tooltip>
-                  <el-tooltip class="item" popper-class="table-tooltip"  effect="dark" content="复制链接" placement="top">
-                    <i class="table-icon fzlj" size="small" @click="handleEdit(item)"></i>
-                  </el-tooltip>
+                  <span class="ctrl">预览</span>
+                  <span class="ctrl">检测</span>
+                  <span class="ctrl" @click="handleUpdateStatus(item)">下架</span>
+                  <span class="ctrl">复制链接</span>
+                  <span class="ctrl">数据</span>
                 </td>
               </tr>
             </tbody>
@@ -100,7 +130,7 @@
         <el-button type="primary"  class="btn-jump-to" @click="jumpTo">跳转</el-button>
           <el-pagination
             background
-            layout="prev, pager, next,jumper"
+            layout="total,prev, pager, next,jumper"
             :current-page="currentPage"
             @current-change="handleCurrentChange"
             :page-size="pageSize"
@@ -122,12 +152,38 @@
     width: 100px;
   }
   .input-with-select{
-    width: 500px;
+    width: 420px;
     .input-left{
-      width: 100px;
+      width: 110px;
     }
     .el-input__icon{
       transition: all 0s;
+    }
+  }
+  .el-dropdown{
+    color: #8A96A0!important;
+    cursor: pointer;
+  }
+  .crm-tag{
+    float: left;
+    height: 24px;
+    font-size: 12px;
+    line-height: 22px;
+    border:1px solid #DCE0E6;
+    border-radius: 2px;
+    color: #5E6D82;
+    margin-top: 8px;
+    margin-right: 20px;
+    .ms{
+      float: left;
+      padding: 0 12px;
+    }
+    .del{
+      float: left;
+      width: 24px;
+      text-align: center;
+      border-left: 1px solid #DCE0E6;
+      cursor: pointer;
     }
   }
 </style>
@@ -142,9 +198,8 @@
                 //表单
                 filters: {
                   status:'0',
-                  name: '',
-                  provence:'',
-                  city:'',
+                  type: 'name',
+                  search:'',
                   time:[]
                 },
                 //table内容
@@ -158,53 +213,99 @@
                 //查询页码
                 page: 1,
                 sels: [],//列表选中列
+                pageType:'',
 
             }
         },
         methods:{
+            //删除标签
+            deleteTag(){
+              this.pageType=''
+              this.search.pageType = this.pageType
+              this.postSearch()
+            },
+            //模糊搜索
+            querySearch(queryString, callback){
+                if(queryString){
+                  let para
+                  if(this.filters.type =="name"){
+                    para = {
+                      "orgName":this.filters.search,
+                      "cityName":this.filters.search,
+                      "projectName":this.filters.search,
+                      "gid":this.filters.search,
+                      "pageName":this.filters.search,
+                      "status":this.filters.status
+                    }
+                  }else{
+                    para = {
+                      [this.filters.type]:this.filters.search,
+                      "status":this.filters.status
+                    }
+                  }
+                  this.$api.requestPagelistSearch(para).then((res)=>{
+                    if(res.code = 1){
+                      if(res.data == undefined){
+                        callback([])
+                      }
+                      else {
+                        let results =  res.data.map((item)=>{
+                          return {"value":item.split(',')[0],'tag':item.split(',')[1]}
+                        })
+                        callback(results)
+                      }
+                    }
+                  })
+                }else{
+                  callback([])
+                }
+            },
+            //规范事件格式
+            configTime(){
+                if(this.filters.time === null  ){
+                  this.filters.time = []
+                }
+                console.log(this.filters.time)
+            },
             //获取用户列表
             getUsers() {
-                console.log(this.filters.time)
+                this.configTime()
                 let para = {
                   "pageNo":this.page,
                   "status":this.filters.status,
                   "name":this.filters.name,
                   "beginDate":this.filters.time[0],
-                  "endDate":this.filters.time[1]
+                  "endDate":this.filters.time[1],
+                  "pageType":this.pageType,
+                  [this.filters.type]:this.filters.search
                 };
-                this.listLoading = true;
-                this.$api.requestPagelist(para).then((res) => {
-                    if(res.code === 1){
-                      console.log(res.data.list)
-                      this.pageList = res.data.list
-                      this.total = res.data.count
-                      this.pageSize = res.data.pageSize
-//                      this.$nextTick(()=>{
-//                        let td = document.getElementsByClassName("page-title")
-//                        console.log(td)
-//                        for(let i = 0; i < td.length ; i++){
-//                          td[i].removeAttribute('title')
-//                          if(td[i].firstChild.innerText.length > 6){
-////                            console.log(td[i].firstChild.innerText)
-//                            td[i].setAttribute('title',td[i].firstChild.innerText)
-//                          }
-//                        }
-//                      })
-                      this.listLoading = false;
-                    }
-                }).catch((err)=>{
-                    this.$confirm("网络报错","提示",{
-                      confirmButtonText: '确定',
-                      cancelButtonText: '取消',
-                      type: 'warning'
-                    }).then(()=>{
-                      this.listLoading = false;
-                    }).catch(()=>{
-                      this.listLoading = false;
-                    })
-                });
+                this.search = para
+                this.postSearch()
             },
-            //查询
+            //传送查询条件
+            postSearch() {
+              this.listLoading = true;
+              this.$api.requestPagelist(this.search).then((res) => {
+                if(res.code === 1){
+                  console.log(res.data.list)
+                  this.pageList = res.data.list
+                  this.total = res.data.count
+                  this.pageSize = res.data.pageSize
+                  this.listLoading = false;
+                }
+              }).catch((err)=>{
+                this.$confirm("网络报错","提示",{
+                  confirmButtonText: '确定',
+                  cancelButtonText: '取消',
+                  type: 'warning'
+                }).then(()=>{
+                  this.listLoading = false;
+                }).catch(()=>{
+                  this.listLoading = false;
+                })
+              });
+            },
+            //查询按钮
             commitForm() {
               this.page = 1
               this.currentPage = 1
@@ -212,18 +313,19 @@
             },
             //radio按钮,重置
             getStatus(){
-              this.filters.name=""
-              this.filters.provence=""
-              this.filters.city=""
+              this.filters.type="name"
+              this.filters.search=""
               this.filters.time=[]
               this.page = 1
               this.currentPage = 1
+              this.pageType=''
               this.getUsers()
             },
             //分页器功能
             handleCurrentChange(val) {
               this.page = val
-              this.getUsers()
+              this.search['pageNo'] = val
+              this.postSearch()
               this.currentPage = val
             },
             //跳转按钮功能
@@ -232,23 +334,44 @@
             },
             //超过十个字添加title属性
             istitle(value){
-
-                if(value.length>6){
+                if(value.length>5){
                   return value
                 }else{
                   return ""
                 }
             },
-            selsChange() {
-
-            },
-            //落地页筛选
-            filterPageType(value, row) {
-              return row.pagetype == value
+            //pageType选择
+            pageTypeHandle(command){
+              this.pageType = command
+              this.getUsers()
             },
             //发布状态筛选
-            handleEdit(value) {
+            handleUpdateStatus(value) {
               console.log(value)
+              this.$confirm('确定下架该落地页?', '提示信息', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+              }).then(() => {
+                console.log(value.uuid)
+//                let _this = this
+                this.$api.pagelistUpdateStatus(value.uuid).then((res)=>{
+                  console.log(res)
+                  if(res.code == 1){
+                    this.$message({
+                      type: 'success',
+                      message: '已下架!'
+                    });
+
+                  }
+                  this.getStatus()
+                })
+              }).catch(() => {
+                this.$message({
+                  type: 'info',
+                  message: '已取消'
+                });
+              });
             },
             handleDel() {
 
@@ -257,21 +380,10 @@
 
             },
 
-            //table菜单下拉查询
-            //落地页类型查询
-            getPageType(item,index) {
-              this.pageTypeindex = index
-              this.isShowpageTypeSlide = false
-            },
-            //当前状态查询
-            getStatusType(item,index) {
-              this.statusTypeIndex = index
-              this.isShowStatusSlide = false
-            }
+
         },
         mounted() {
           this.getUsers()
         }
-
     }
 </script>
